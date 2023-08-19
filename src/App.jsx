29560a80841bfd01505bffba69f5e270a9580a7e
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-08-20 01:51:47 +0400

    Tailwind Css Configure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold bg-amber-100">
+            Hello world!
+        </h1>
+    </div>
+  );
+};
+
+export default App;
