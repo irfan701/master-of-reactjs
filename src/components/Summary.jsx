5641adee571e2a80380154e000646e61c6b725e0
irfan701 <34702754+irfan701@users.noreply.github.com>
2023-08-27 00:03:10 +0400

    React CountUp with summary cp

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,4 +1,6 @@
 import { FaCheckCircle } from "react-icons/fa";
+import Countup from "./utils/Countup.jsx";
+
 const Summary = () => {
     return (
         <>
@@ -9,16 +11,16 @@ const Summary = () => {
                         <div className='grid grid-cols-3 gap-4 text-white text-center mt-[10%]'>
 
                             <div className='mt-12'>
-                                <h1 className='countNumber'>100</h1>
+                                <h1 className='countNumber'>{<Countup start={0} end={100}/>}</h1>
                                 <h4 className='countTitle'>Total Projects</h4>
                                 <hr className='w-28 mx-auto'/>
                             </div>
 
                             <div className='mt-12'>
-                                <h1 className='countNumber'>100</h1>
+                                <h1 className='countNumber'>{<Countup start={0} end={100}/>}</h1>
                                 <h4 className='countTitle'>Total Clients</h4>
                                 <hr className='w-28 mx-auto'/>
-                            </div>
+                            </div>.
 
                             <div className='card workCard'>
                                 <h1 className='cardTitle text-justify'>How I Work </h1>
